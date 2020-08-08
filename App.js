b88081a64fe00fd2e721058d26b1cffc51b51c63
youngsik_won <youngsik3200@gmail.com>
2020-08-09 03:19:10 +0900

    Props

--- App.js
+++ App.js
@@ -6,12 +6,13 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
+  Image,
   Text,
   StatusBar,
 } from 'react-native';
@@ -23,14 +24,24 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+class SYJ extends Component{
+    render() {
+        return (
+            <View>
+                <Image source ={require('./assets/SYJ.jpg')}
+                style={{width : 200, height : 250}}/>
+
+            </View>
+        );
+    }
+}
+
 
 const App: () => React$Node = () => {
   return (
       <View style = {styles.container}>
-        <Text>
-          hello world
-        </Text>
-
+        <Text style = {styles.hello}>hello world good night</Text>
+          <SYJ/>
       </View>
   );
 }
@@ -40,7 +51,14 @@ const styles = StyleSheet.create({
  flex : 1,
   justifyContent : "center",
     alignItems: "center"
-  }
+  },
+    hello : {
+
+      color : 'blue',
+        fontWeight : 'bold',
+        fontSize : 30
+
+    }
 });
 
 export default App;
